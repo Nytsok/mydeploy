546a5ee621f8f19c13a7d05db6af0afc58c09598
Nytsok <64953453+Nytsok@users.noreply.github.com>
2025-12-23 14:22:40 +0100

    Replace openfortivpn with obs-studio in install command

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stabl
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install tar bzip2 ansible libreoffice openfortivpn  -y
+sudo apt install tar bzip2 ansible libreoffice openfortivpn obs-studio  -y
 ansible-playbook -K playbook.yml
