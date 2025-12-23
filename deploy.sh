200ea615fd2a48ee318ab6c9a15e2ceef1de1ba3
Nytsok <64953453+Nytsok@users.noreply.github.com>
2025-12-23 14:20:58 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install tar bzip2 ansible libreoffice -y
+sudo apt install tar bzip2 ansible libreoffice openfortivpn  -y
 ansible-playbook -K playbook.yml
